9d3623d277d6cf6f1436ecbb491aa1794bfe145f
Malik Ismail <malik@emq.com>
2026-06-09 13:37:46 +0800

    fix: introduced calculator bug

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def add_numbers(a, b):
-    return a + b  #  Bug fixed!
+    return a - b  # ❌ Bug introduced again
 
 if __name__ == "__main__":
     print(f"Result of 5 + 10 is: {add_numbers(5, 10)}")
